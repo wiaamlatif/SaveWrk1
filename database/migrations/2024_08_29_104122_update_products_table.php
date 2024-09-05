3d2aa151d6739c931ba28719ff29396158abfcf3
latif <112183602+wiaamlatif@users.noreply.github.com>
2024-09-05 03:26:34 -0700

    Add files via upload

---
+++ database/migrations/2024_08_29_104122_update_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignIdFor(Category::class)
+                    ->nullable() //le champs peut etre nul
+                    ->constrained()
+                    ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeignIdFor(Category::class);
+
+        });
+    }
+};
